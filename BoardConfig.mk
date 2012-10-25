ca19b78381b2d00d785f8a994f854c4995cce22b
Hashcode <hashcode0f@gmail.com>
2012-10-24 22:36:39 -0700

    simplify build
    Change-Id: I73bdef754dcb50356cd4e13581fa196d5b74b9d8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,49 +1,29 @@
-# Processor
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_SMP := true
-TARGET_ARCH_VARIANT := armv7-a-neon
-ARCH_ARM_HAVE_TLS_REGISTER := true
+# inherit from common
+-include device/safestrap-common/BoardConfigCommon.mk
 
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_CMDLINE := mem=512M console=ttyO2,115200n8
-TARGET_NO_RADIOIMAGE := true
 TARGET_BOARD_PLATFORM := omap4
-TARGET_NO_BOOTLOADER := true
 TARGET_BOOTLOADER_BOARD_NAME := otter
 TARGET_OTA_ASSERT_DEVICE := blaze
-TARGET_BOARD_INFO_FILE := device/amazon/otter/board-info.txt
 BOARD_HAS_SDCARD_INTERNAL := true
-BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/platform/omap/omap_hsmmc.1/by-name/media
-BOARD_SDCARD_DEVICE_INTERNAL := /dev/block/platform/omap/omap_hsmmc.1/by-name/media
 
 # Build init which doesn't remount /dev, /proc and /sys for 2nd-init
 BOARD_HAS_LOCKED_BOOTLOADER := true
 
-# Recovery
-TARGET_PREBUILT_KERNEL := device/generic/recovery/kernel
-BOARD_ALWAYS_INSECURE := true
-BOARD_HAS_LARGE_FILESYSTEM := true
-
 # This needs to be a build.prop
 BOARD_UMS_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun/file"
 
-# TWRP
-TARGET_RECOVERY_INITRC := device/generic/recovery/root/init.rc
+# TWRP/Recovery
+TARGET_RECOVERY_INITRC := device/generic/recovery/init.rc
+TARGET_PROVIDES_INIT_RC := true
 TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
 DEVICE_RESOLUTION := 1024x600
 TW_INTERNAL_STORAGE_PATH := "/sdcard"
 TW_INTERNAL_STORAGE_MOUNT_POINT := "sdcard"
-TW_NO_REBOOT_BOOTLOADER := true
-TW_NO_REBOOT_RECOVERY := true
-TW_FLASH_FROM_STORAGE := true
-TW_CUSTOM_POWER_BUTTON := 107
+TARGET_USERIMAGES_USE_EXT4 := true
 RECOVERY_TOUCHSCREEN_SWAP_XY := true
 RECOVERY_TOUCHSCREEN_FLIP_Y := true
-TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_PROVIDES_INIT_RC := true
 
-# Debugging
-#TWRP_EVENT_LOGGING := true
