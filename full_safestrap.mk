cbd9260087e5ba3dde8bbcb1764533b05787e3dc
Hashcode <hashcode0f@gmail.com>
2012-10-31 00:19:59 -0700

    revert to setup_fs hijack
    Change-Id: I690937bbca5e31a5a5e7d47a1214e64d38c272e2

--- full_safestrap.mk
+++ full_safestrap.mk
@@ -2,6 +2,8 @@
 
 DEVICE_FOLDER := device/generic/safestrap
 COMMON_FOLDER := device/generic/safestrap-common
+HIJACK_BIN := setup_fs
+#HIJACK_BIN := start_smc.sh
 
 # Only uncomment for testing on 1stgen KFire
 USE_KFIRE_TEST := true
@@ -18,7 +20,6 @@ PRODUCT_COPY_FILES += \
 # Device specific 2nd-init files
 PRODUCT_COPY_FILES += \
     $(DEVICE_FOLDER)/sbin/fixboot.sh:$(TARGET_RECOVERY_OUT)/root/../2nd-init-files/fixboot.sh \
-    $(DEVICE_FOLDER)/sbin/hijack.killall:$(TARGET_RECOVERY_OUT)/root/../2nd-init-files/hijack.killall \
 
 ifdef USE_KFIRE_TEST
 # Safestrapmenu
@@ -28,16 +29,11 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_FOLDER)/res_1024x600/background-safe.png:$(TARGET_RECOVERY_OUT)/root/../install-files/etc/safestrap/res/background-safe.png \
     $(DEVICE_FOLDER)/res_1024x600/splashkeys.ilitek_i2c:$(TARGET_RECOVERY_OUT)/root/../install-files/etc/safestrap/res/splashkeys.ilitek_i2c \
 
-# 2nd-init stock rootfs files [1st gen KFire for testing]
-#PRODUCT_COPY_FILES += \
-#    $(DEVICE_FOLDER)/root/init.omap4430.rc-1stgen:$(TARGET_RECOVERY_OUT)/root/../install-files/etc/safestrap/rootfs/init.omap4430.rc \
-#    $(DEVICE_FOLDER)/root/init.rc-1stgen:$(TARGET_RECOVERY_OUT)/root/../install-files/etc/safestrap/rootfs/init.rc
-
 # App files
 PRODUCT_COPY_FILES += \
-    $(COMMON_FOLDER)/app/omap4-amazon-start_smc/recovery-check.sh:$(TARGET_RECOVERY_OUT)/root/../APP/recovery-check.sh \
-    $(COMMON_FOLDER)/app/omap4-amazon-start_smc/recovery-install.sh-otter:$(TARGET_RECOVERY_OUT)/root/../APP/recovery-install.sh \
-    $(COMMON_FOLDER)/app/omap4-amazon-start_smc/recovery-uninstall.sh:$(TARGET_RECOVERY_OUT)/root/../APP/recovery-uninstall.sh
+    $(COMMON_FOLDER)/app/omap4-amazon-$(HIJACK_BIN)/recovery-check.sh:$(TARGET_RECOVERY_OUT)/root/../APP/recovery-check.sh \
+    $(COMMON_FOLDER)/app/omap4-amazon-$(HIJACK_BIN)/recovery-install.sh-otter:$(TARGET_RECOVERY_OUT)/root/../APP/recovery-install.sh \
+    $(COMMON_FOLDER)/app/omap4-amazon-$(HIJACK_BIN)/recovery-uninstall.sh:$(TARGET_RECOVERY_OUT)/root/../APP/recovery-uninstall.sh
 else
 # Safestrapmenu
 PRODUCT_COPY_FILES += \
@@ -46,22 +42,18 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_FOLDER)/res_1280x800/background-safe.png:$(TARGET_RECOVERY_OUT)/root/../install-files/etc/safestrap/res/background-safe.png \
     $(DEVICE_FOLDER)/res_1280x800/splashkeys.AtmelTouch:$(TARGET_RECOVERY_OUT)/root/../install-files/etc/safestrap/res/splashkeys.AtmelTouch \
 
-# 2nd-init stock rootfs files [KFire-HD7 files]
-#PRODUCT_COPY_FILES += \
-#    $(DEVICE_FOLDER)/root/init.bowser.rc-kfhd:$(TARGET_RECOVERY_OUT)/root/../install-files/etc/safestrap/rootfs/init.bowser.rc \
-#    $(DEVICE_FOLDER)/root/init.rc-kfhd:$(TARGET_RECOVERY_OUT)/root/../install-files/etc/safestrap/rootfs/init.rc
-
 # App files
 PRODUCT_COPY_FILES += \
-    $(COMMON_FOLDER)/app/omap4-amazon-start_smc/recovery-check.sh:$(TARGET_RECOVERY_OUT)/root/../APP/recovery-check.sh \
-    $(COMMON_FOLDER)/app/omap4-amazon-start_smc/recovery-install.sh-bowser:$(TARGET_RECOVERY_OUT)/root/../APP/recovery-install.sh \
-    $(COMMON_FOLDER)/app/omap4-amazon-start_smc/recovery-uninstall.sh:$(TARGET_RECOVERY_OUT)/root/../APP/recovery-uninstall.sh
+    $(COMMON_FOLDER)/app/omap4-amazon-$(HIJACK_BIN)/recovery-check.sh:$(TARGET_RECOVERY_OUT)/root/../APP/recovery-check.sh \
+    $(COMMON_FOLDER)/app/omap4-amazon-$(HIJACK_BIN)/recovery-install.sh-bowser:$(TARGET_RECOVERY_OUT)/root/../APP/recovery-install.sh \
+    $(COMMON_FOLDER)/app/omap4-amazon-$(HIJACK_BIN)/recovery-uninstall.sh:$(TARGET_RECOVERY_OUT)/root/../APP/recovery-uninstall.sh
 endif
 
 
 # Choose a Common Hijack
 PRODUCT_COPY_FILES += \
-    $(COMMON_FOLDER)/hijacks/start_smc.sh-kfire:$(TARGET_RECOVERY_OUT)/root/../install-files/bin/start_smc.sh \
+    $(COMMON_FOLDER)/hijacks/$(HIJACK_BIN)-kfire:$(TARGET_RECOVERY_OUT)/root/../install-files/bin/$(HIJACK_BIN) \
+    $(COMMON_FOLDER)/hijacks/$(HIJACK_BIN)-hijack-kfire:$(TARGET_RECOVERY_OUT)/root/../install-files/bin/$(HIJACK_BIN)-hijack \
 
 $(call inherit-product, $(COMMON_FOLDER)/common.mk)
 
