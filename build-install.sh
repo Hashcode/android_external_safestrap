d508fcd0fbb91eab34ac4e90e8f40ba3b0652d8f
Hashcode <hashcode0f@gmail.com>
2012-11-04 08:55:36 -0800

    add stock-modified rootfs files
    Change-Id: Ib913b91fb5188cbc8d5be597d749e33d72ce237e

--- build-install.sh
+++ build-install.sh
@@ -16,6 +16,11 @@ touch init.mapphone_umts.rc
 cp $ANDROID_BUILD_TOP/device/generic/safestrap/default.prop default.prop
 rm $OUT/install-files/etc/safestrap/recovery.zip
 zip -9r $OUT/install-files/etc/safestrap/recovery .
+mkdir $OUT/install-files/etc/safestrap/rootfs
+rm $OUT/install-files/etc/safestrap/rootfs/init
+cp $OUT/recovery/root/init $OUT/install-files/etc/safestrap/rootfs/
+chmod 755 $OUT/install-files/etc/safestrap/rootfs/init
+cp $ANDROID_BUILD_TOP/device/generic/safestrap/init.rc $OUT/install-files/etc/safestrap/rootfs/
 cd $OUT
 zip -9r APP/install-files install-files
 
