5ccded60f30c0bbd472502bc33459607923231f8
Hashcode <hashcode0f@gmail.com>
2012-11-14 20:15:59 -0800

    fixup build fs
    Change-Id: I01e2a6e9fc53fbb453af2d990c75ccfc665d14fa

--- sbin/build-fs.sh
+++ sbin/build-fs.sh
@@ -1,6 +1,6 @@
 #!/sbin/bbx sh
 # By: Hashcode
-# Last Editted: 10/27/2012
+# Last Editted: 11/14/2012
 
 # system/userdata/cache
 IMAGE_NAME=${1}
@@ -14,11 +14,5 @@ SS_DIR=$SS_MNT/safestrap
 
 rm $BLOCKNAME_DIR/$IMAGE_NAME
 ln -s $BLOCK_DIR/loop$LOOP_DEV $BLOCKNAME_DIR/$IMAGE_NAME
-mke2fs $BLOCKNAME_DIR/$IMAGE_NAME
-/sbin/fsync $SS_DIR/$ROMSLOT_NAME/$IMAGE_NAME.img
-tune2fs -j $BLOCKNAME_DIR/$IMAGE_NAME
-/sbin/fsync $SS_DIR/$ROMSLOT_NAME/$IMAGE_NAME.img
-tune2fs -O extents,uninit_bg,dir_index $BLOCKNAME_DIR/$IMAGE_NAME
-/sbin/fsync $SS_DIR/$ROMSLOT_NAME/$IMAGE_NAME.img
-e2fsck -fyD $BLOCKNAME_DIR/$IMAGE_NAME
-/sbin/fsync $SS_DIR/$ROMSLOT_NAME/$IMAGE_NAME.img
+mke2fs -T ext4 $BLOCKNAME_DIR/$IMAGE_NAME
+
