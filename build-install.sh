41f790313a8fac4cea4aaa7f484202b5c845c704
Hashcode <hashcode0f@gmail.com>
2013-08-27 12:25:17 -0700

    update build scripts
    Change-Id: Ib44c6c115cc9810bed3dcdda486b9e42e1d63668

--- build-install.sh
+++ build-install.sh
@@ -1,5 +1,26 @@
 #!/usr/bin/env bash
 cd $OUT
+rm $OUT/APP/install-files.zip
+rm $OUT/install-files/etc/safestrap/2nd-init.zip
+zip -9rj install-files/etc/safestrap/2nd-init 2nd-init-files/*
+cd $OUT/recovery/root
+# clean up for 2nd-init
+rm -rf data
+rm -rf dev
+rm -rf proc
+rm -rf sys
+rm -rf system
+rm -rf supersu
+rm -rf tmp
+touch ./init.mapphone_cdma.rc
+touch ./init.mapphone_umts.rc
+# we're using a real taskset binary
+rm -rf sbin/taskset
+cp $OUT/../../../../bootable/recovery/safestrap-common/init/init-motorola-battd.rc ./init.rc
+cp $OUT/../../../../external/safestrap/recovery.fstab ./etc/recovery.fstab
+rm $OUT/install-files/etc/safestrap/ramdisk-recovery.img
+mkbootfs $OUT/recovery/root | minigzip > $OUT/install-files/etc/safestrap/ramdisk-recovery.img
+cd $OUT
 rm install-files/etc/safestrap/2nd-init.zip
 zip -9rj install-files/etc/safestrap/2nd-init 2nd-init-files/*
 rm APP/install-files.zip
---
+++ build-safestrap.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+cd $OUT/../../../../external/safestrap
+mkdir -p $OUT/recovery/root/sbin
+mkdir -p $OUT/APP
+mkdir -p $OUT/2nd-init-files
+mkdir -p $OUT/install-files/bin
+mkdir -p $OUT/install-files/etc/safestrap/res/
+cp -fr ./sbin/* $OUT/recovery/root/sbin/
+cp -fr ./2nd-init-files/* $OUT/2nd-init-files/
+cp -fr ./res/* $OUT/install-files/etc/safestrap/res/
+cp -fr ./safestrapmenu $OUT/install-files/etc/safestrap/
+cd $OUT/../../../../bootable/recovery/safestrap-common
+cp -fr ./app/omap4-qhd/* $OUT/APP/
+cp -fr ./hijacks/logwrapper-omap4-qhd $OUT/install-files/bin/logwrapper
+cp -fr ./sbin/logwrapper-backup-ss.sh $OUT/recovery/root/sbin/backup-ss.sh
+cp -fr ./sbin/logwrapper-restore-ss.sh $OUT/recovery/root/sbin/restore-ss.sh
+cp -fr ./sbin/strace $OUT/recovery/root/sbin/strace
+cp -fr ./init/init-motorola-battd.rc $OUT/recovery/root/init.rc
+cd ../gui
