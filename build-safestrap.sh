0d8cd648dc9c36fdef10c309eadd982673649088
Hashcode <hashcode0f@gmail.com>
2013-09-01 14:14:52 -0700

    update build for generic qcom hijack
    Change-Id: I9a47af2e486688eac3f478aa2e3f9a8374d6058b

--- build-safestrap.sh
+++ build-safestrap.sh
@@ -3,17 +3,16 @@ cd $OUT/../../../../external/safestrap
 mkdir -p $OUT/recovery/root/sbin
 mkdir -p $OUT/APP
 mkdir -p $OUT/2nd-init-files
-mkdir -p $OUT/install-files/bin
 mkdir -p $OUT/install-files/etc/safestrap/res/
 cp -fr ./sbin/* $OUT/recovery/root/sbin/
 cp -fr ./2nd-init-files/* $OUT/2nd-init-files/
 cp -fr ./res/* $OUT/install-files/etc/safestrap/res/
-cd $OUT/../../../../bootable/recovery/safestrap-common/devices/motorola_qcom-common
-cp -fr ../safestrapmenu $OUT/install-files/etc/safestrap/
+cd $OUT/../../../../bootable/recovery/safestrap-common/devices/motorola_msm8960-common
+#cp -fr ../safestrapmenu $OUT/install-files/etc/safestrap/
 cp -fr ./recovery-check.sh $OUT/APP/
 cp -fr ./recovery-install.sh $OUT/APP/
 cp -fr ./recovery-uninstall.sh $OUT/APP/
-cp -fr ./hijack $OUT/install-files/bin/logwrapper
+cp -fr ./hijack $OUT/install-files/etc/init.qcom.modem_links.sh
 cp -fr ./backup-ss.sh $OUT/recovery/root/sbin/backup-ss.sh
 cp -fr ./restore-ss.sh $OUT/recovery/root/sbin/restore-ss.sh
 cd ../../../gui
