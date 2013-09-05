b3c00d96efae6fbbc5fdbd03ea8a31066d9fd298
Hashcode <hashcode0f@gmail.com>
2013-09-05 00:06:53 -0700

    add recovery-reboot.sh to build
    Change-Id: Id480da6fff02431ab8b2a6a39325d9d146d0c4b1

--- build-safestrap.sh
+++ build-safestrap.sh
@@ -13,6 +13,7 @@ cd $OUT/../../../../bootable/recovery/safestrap-common/devices/motorola_omap4-co
 cp -fr ./recovery-check.sh $OUT/APP/
 cp -fr ./recovery-install.sh $OUT/APP/
 cp -fr ./recovery-uninstall.sh $OUT/APP/
+cp -fr ./recovery-reboot.sh $OUT/APP/
 cp -fr ./hijack $OUT/install-files/bin/logwrapper
 cp -fr ./backup-ss.sh $OUT/recovery/root/sbin/backup-ss.sh
 cp -fr ./restore-ss.sh $OUT/recovery/root/sbin/restore-ss.sh
