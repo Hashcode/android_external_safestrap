713447d75f1c2c0c7988c3c9718f6ac6081fc772
Hashcode <hashcode0f@gmail.com>
2013-09-06 09:19:58 -0700

    clear out more files
    Change-Id: I25bb2837e927f0d78d46746ffad1c7fc55eddffe

--- build-install.sh
+++ build-install.sh
@@ -12,10 +12,21 @@ rm -rf sys
 rm -rf system
 rm -rf supersu
 rm -rf tmp
+# erase custom .rc files
+touch ./MSM8960_lpm.rc
+touch ./init.carrier.rc
 touch ./init.mmi.usb.rc
 touch ./init.qcom.rc
+touch ./init.qcom.usb.rc
 touch ./init.target.rc
 touch ./init.vzw.rc
+touch ./lpm.rc
+# erase SElinux files
+touch ./file_contexts
+touch ./property_contexts
+touch ./seapp_contexts
+touch ./sepolicy
+touch ./sepolicy_version
 # we're using a real taskset binary
 rm -rf sbin/taskset
 cp $OUT/../../../../bootable/recovery/safestrap-common/devices/samsung_msm8960-common/init.rc ./init.rc
