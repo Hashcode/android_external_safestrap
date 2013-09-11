dfc5264779f3906111db65a19cffa7cfec6f2f79
Hashcode <hashcode0f@gmail.com>
2013-09-11 10:32:16 -0700

    update safestrap build: turn back on crypto, enable backlight, disable loggin, dont double-buffer
    Change-Id: I740992d24a54b6a75ea42db01c5d5e8339e84e64

--- safestrap.mk
+++ safestrap.mk
@@ -2,6 +2,8 @@
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 DEVICE_RESOLUTION := 1080x1920
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
+RECOVERY_GRAPHICS_DONT_DOUBLE_BUFFER := true
+
 RECOVERY_SDCARD_ON_DATA := true
 TW_INTERNAL_STORAGE_PATH := "/datamedia/media/0"
 TW_INTERNAL_STORAGE_MOUNT_POINT := "datamedia"
@@ -11,25 +13,22 @@ TW_NO_REBOOT_BOOTLOADER := true
 TW_HAS_DOWNLOAD_MODE := true
 BOARD_HAS_NO_REAL_SDCARD := true
 
-#HASH: disable CRYPTO for now in recovery
-#TW_INCLUDE_CRYPTO := true
-#TW_INCLUDE_CRYPTO_SAMSUNG := true
-#TW_CRYPTO_FS_TYPE := "ext4"
-#TW_CRYPTO_REAL_BLKDEV := "/dev/block/mmcblk0p29"
-#TW_CRYPTO_MNT_POINT := "/data"
-#TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,noatime,noauto_da_alloc,discard,journal_async_commit,errors=panic wait,check,encryptable=footer"
-#TW_CRYPTO_FS_FLAGS := "0x00000406"
-#TW_CRYPTO_KEY_LOC := "footer"
-
-TW_BRIGHTNESS_PATH := /sys/class/backlight/panel/brightness
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun0/file
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_CRYPTO_SAMSUNG := true
+TW_CRYPTO_FS_TYPE := "ext4"
+TW_CRYPTO_REAL_BLKDEV := "/dev/block/mmcblk0p29"
+TW_CRYPTO_MNT_POINT := "/data"
+TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,noatime,noauto_da_alloc,discard,journal_async_commit,errors=panic wait,check,encryptable=footer"
+TW_CRYPTO_FS_FLAGS := "0x00000406"
+TW_CRYPTO_KEY_LOC := "footer"
 
 TW_INCLUDE_SPLASHMENU := true
 TW_DEFAULT_EXTERNAL_STORAGE := true
 TARGET_USERIMAGES_USE_EXT4 := true
+TW_BRIGHTNESS_PATH := /sys/class/lcd/panel/backlight
 
 # Logging
-TWRP_EVENT_LOGGING := true
+#TWRP_EVENT_LOGGING := true
 
 # Virtual partition size default (in mb)
 BOARD_DEFAULT_VIRT_SYSTEM_SIZE := 1024
