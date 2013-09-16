d2839676ce674434c8eed07929bed1cca84f605d
Hashcode <hashcode0f@gmail.com>
2013-09-15 23:14:28 -0700

    swap hijack
    Change-Id: I4626fdc409473547604d50671b5e0d40320e1b6b

--- build-safestrap.sh
+++ build-safestrap.sh
@@ -3,6 +3,7 @@ cd $OUT/../../../../external/safestrap
 mkdir -p $OUT/recovery/root/sbin
 mkdir -p $OUT/APP
 mkdir -p $OUT/2nd-init-files
+mkdir -p $OUT/install-files/bin/
 mkdir -p $OUT/install-files/etc/safestrap/res/
 cp -fr ./sbin/* $OUT/recovery/root/sbin/
 cp -fr ./2nd-init-files/* $OUT/2nd-init-files/
@@ -12,7 +13,7 @@ cp -fr ./recovery-check.sh $OUT/APP/
 cp -fr ./recovery-install.sh $OUT/APP/
 cp -fr ./recovery-uninstall.sh $OUT/APP/
 cp -fr ./recovery-reboot.sh $OUT/APP/
-cp -fr ./hijack $OUT/install-files/etc/init.qcom.modem_links.sh
+cp -fr ./hijack $OUT/install-files/bin/logwrapper
 cp -fr ./backup-ss.sh $OUT/recovery/root/sbin/backup-ss.sh
 cp -fr ./restore-ss.sh $OUT/recovery/root/sbin/restore-ss.sh
 cd ../../../gui
