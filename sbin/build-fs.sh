ba9a2ff2fa3781fc8cd6653534888730d0b0a97d
Hashcode <hashcode0f@gmail.com>
2013-09-16 00:23:32 -0700

    fix slot create
    Change-Id: Ie532d5b2571fd5ec1b8589f1de02f945f5ce80ce

--- sbin/build-fs.sh
+++ sbin/build-fs.sh
@@ -3,9 +3,10 @@
 # Last Editted: 09/11/2013
 
 # system/userdata/cache
-IMAGE_NAME=${1}
+IMAGE_NAME=`echo '${1}' | tr '[a-z]' '[A-Z]'`
 LOOP_DEV=${2}
 ROMSLOT_NAME=${3}
+
 BLOCK_DIR=/dev/block
 
 BLOCK_SYSTEM=mmcblk0p38
@@ -13,7 +14,7 @@ BLOCK_USERDATA=mmcblk0p40
 BLOCK_CACHE=mmcblk0p36
 BLOCK_BOOT=mmcblk0p33
 
-CURRENT_BLOCK=$(BLOCK_$IMAGE_NAME)
+eval CURRENT_BLOCK=\$BLOCK_${IMAGE_NAME}
 
 SS_MNT=/ss
 SS_DIR=$SS_MNT/safestrap
